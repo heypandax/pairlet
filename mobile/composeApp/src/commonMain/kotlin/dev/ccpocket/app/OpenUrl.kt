1bd23cfb17a62160fb895a6abddf6dc86f63c85e
panda <pandaleecn@gmail.com>
2026-07-25 05:27:44 -0700

    fix: bust stale support page caches

--- mobile/composeApp/src/commonMain/kotlin/dev/ccpocket/app/OpenUrl.kt
+++ mobile/composeApp/src/commonMain/kotlin/dev/ccpocket/app/OpenUrl.kt
@@ -2,7 +2,8 @@ package dev.ccpocket.app
 
 const val USER_MANUAL_URL = "https://heypandax.github.io/cc-pocket/manual/"
 const val USER_MANUAL_TROUBLESHOOTING_URL = "${USER_MANUAL_URL}?q=offline"
-const val SUPPORT_URL = "https://heypandax.github.io/cc-pocket/support/"
+// Version the public entry so in-app browser/CDN caches cannot strand users on a retired support channel.
+const val SUPPORT_URL = "https://heypandax.github.io/cc-pocket/support/?v=20260725-2"
 
 /** Open an http(s) [url] for viewing: mobile presents an in-app browser (iOS SFSafariViewController,
  *  Android a Custom Tab when the default browser offers one), desktop hands it to the system browser. */
